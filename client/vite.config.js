ca02b1a14842d01bcf08566d42a44c7f6f79b466
vanshilgabani <vanshilgabani@gmail.com>
2026-01-12 11:04:47 +0530

    VeeRaa logo browser icon  deployment for Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public', // ✅ Explicitly set
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
     proxy: {
